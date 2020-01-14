0d0890883fe8074b76e41ef697abbaee554b458f
erikos93 <erikwaahlstrom@gmail.com>
2020-01-14 14:13:45 +0100

    basic setup

---
+++ src/components/InputBar.vue
@@ -0,0 +1,10 @@
+<template>
+  <input value="Write things todo" />
+</template>
+
+<script>
+export default {};
+</script>
+
+<style>
+</style>
---
+++ src/components/TodoList.vue
@@ -0,0 +1,13 @@
+<template>
+  <ul>
+    <li>List item</li>
+    <li>List item</li>
+  </ul>
+</template>
+
+<script>
+export default {};
+</script>
+
+<style>
+</style>
