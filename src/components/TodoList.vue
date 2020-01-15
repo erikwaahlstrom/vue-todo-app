5b8da2540dd191c62bef1b4a50f9bd057138591c
Erik Wahlström <erik93_93@hotmail.com>
2020-01-15 12:04:39 +0100

    delete item

--- src/components/TodoList.vue
+++ src/components/TodoList.vue
@@ -8,7 +8,10 @@
       @keyup.enter="addTodo"
     />
     <ul>
-      <li v-for="todo in todos" :key="todo.id" class="todo-item">{{todo.title}}</li>
+      <div v-for="(todo, index) in todos" :key="todo.id" class="todo-item">
+        <li>{{todo.title}}</li>
+        <div class="remove-item" @click="removeTodo(index)">&times;</div>
+      </div>
     </ul>
   </div>
 </template>
@@ -18,23 +21,39 @@ export default {
   data() {
     return {
       newTodo: "",
+      idForTodo: 3,
       todos: [
         {
           id: 1,
           title: "Finish vue screencast",
-          boolean: false
+          completed: false
         },
         {
           id: 2,
           title: "Make food",
-          boolean: false
+          completed: false
         }
       ]
     };
   },
   methods: {
     addTodo() {
-     
+      if (this.newTodo.trim().length == 0) {
+        return;
+      }
+      this.todos.push({
+        id: this.idForTodo,
+        title: this.newTodo,
+        completed: false
+      });
+
+      this.newTodo = "";
+      this.idForTodo++;
+    },
+    // V-for skapar ett index för varje element som skapas. Klick event tar sedan med sig denna unika index till removeTodo funktionen. Där väljer vi ut vilken array som vi ska redigera och sedan vilket object i ordningen som ska tas bort.
+    removeTodo(index) {
+      console.log(index);
+      this.todos.splice(index, 1);
     }
   }
 };
@@ -45,10 +64,27 @@ export default {
   border: 2px solid blue;
 }
 
+.todo-item {
+  font-size: 20px;
+  margin-bottom: 14px;
+  list-style: none;
+  display: flex;
+  align-items: center;
+  justify-content: space-between;
+}
+
 .todo-input {
   width: 100%;
   padding: 10px 18px;
   font-size: 18px;
   margin-bottom: 16px;
+}
+
+.remove-item {
+  cursor: pointer;
+}
+
+.remove-item:hover {
+  background-color: #e8e8e8;
 }
 </style>
